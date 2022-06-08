fc0491073f89a555ab8b77661de7fa2cfa1f4a66
Dayan Eam <eam.dayan@gmail.com>
2022-06-08 17:57:11 +0700

    update oidc shellscript

--- kong/enableOidc.sh
+++ kong/enableOidc.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# curl -X DELETE 'http://localhost:8001/services/test-api/plugins/'
-
 export HOST="http://localhost:8800/auth/realms/myrealm/.well-known/openid-configuration"
 
-curl -X POST \
-  --url http://localhost:8001/services/drive-service/plugins \
+# curl -X POST \
+#   --url http://localhost:8001/services/drive-service/plugins \
+#   --data "name=oidc" \
+#   --data "config.client_id=myclient" \
+#   --data "config.client_secret=jAm3yNm3VkfK8oXgU5SNJGZmO8GK80QQ" \
+#   --data "config.discovery=$HOST" \
+#   --data "config.realm=myrealm"
+
+curl -s -X POST http://saintrivers.tech:8001/plugins \
   --data "name=oidc" \
   --data "config.client_id=myclient" \
-  --data "config.client_secret=jAm3yNm3VkfK8oXgU5SNJGZmO8GK80QQ" \
-  --data "config.discovery=$HOST" \
+  --data "config.client_secret=xmmLi7dxS23QxdrvV89ilxzGUYKEa9BM" \
+  --data "config.discovery=https://saintrivers.tech:8443/auth/realms/myrealm/.well-known/openid-configuration"  \
   --data "config.realm=myrealm"
-
-# this redirects properly but google cannot redirect to a private ip address
-# 172.26.0.5
-# this one is the private ip of the docker container
-
-# 128.199.164.171
-# this is the server of my ip
